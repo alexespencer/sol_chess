89e43686caa18581e5b6b66c813332fba7fec994
cool-mist <n.suryap@gmail.com>
2025-02-23 15:31:59 +0530

    Do the actual deployment using sudo

--- deploy-ltpd.sh
+++ deploy-ltpd.sh
@@ -2,9 +2,9 @@
 
 serve_root=$1
 
-mv ./sol_chess.tar.gz $serve_root/sol_chess.tar.gz && \
-tar -xzvf $serve_root/sol_chess.tar.gz -C $serve_root && \
-rm $serve_root/sol_chess.tar.gz
+sudo mv ./sol_chess.tar.gz $serve_root/sol_chess.tar.gz && \
+sudo tar -xzvf $serve_root/sol_chess.tar.gz -C $serve_root && \
+sudo rm $serve_root/sol_chess.tar.gz
 
 echo "Deployment complete"
 
