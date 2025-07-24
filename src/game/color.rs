0b132f71536309d2d473df97afaae7f68fdd8bb4
Alex Spencer <alex@Mac.broadband>
2025-07-24 20:08:15 +0100

    Remove dead code

--- src/game/color.rs
+++ src/game/color.rs
@@ -2,36 +2,28 @@ use macroquad::prelude::*;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum UiColor {
-    #[allow(dead_code)]
-    Grey,
     Green,
     Pink,
     Brown,
     Yellow,
-    #[allow(dead_code)]
-    Blue,
 }
 
 impl UiColor {
     pub fn to_bg_color(&self) -> Color {
         match self {
-            UiColor::Grey => Color::from_rgba(140, 140, 140, 200),
             UiColor::Green => Color::from_rgba(16, 60, 50, 200),
             UiColor::Pink => Color::from_rgba(234, 128, 71, 200),
             UiColor::Brown => Color::from_rgba(123, 61, 35, 200),
             UiColor::Yellow => Color::from_rgba(242, 230, 190, 200),
-            UiColor::Blue => Color::from_rgba(47, 85, 172, 200),
         }
     }
 
     pub fn to_fg_color(&self) -> Color {
         match self {
-            UiColor::Grey => Color::from_rgba(255, 255, 255, 200),
             UiColor::Green => Color::from_rgba(255, 255, 255, 200),
             UiColor::Pink => Color::from_rgba(255, 255, 255, 200),
             UiColor::Brown => Color::from_rgba(255, 255, 255, 200),
             UiColor::Yellow => Color::from_rgba(0, 0, 0, 200),
-            UiColor::Blue => Color::from_rgba(255, 255, 255, 200),
         }
     }
 
